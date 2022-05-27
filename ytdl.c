d0ad42af46d9a9c52ae7d76dbbb35c4f9618d0a5
VinceZCL <64774232+VinceZCL@users.noreply.github.com>
2022-05-27 15:47:50 +0800

    debugged ytdl.c

--- ytdl.c
+++ ytdl.c
@@ -14,26 +14,30 @@ int main() {
 	int c = burner();
 
 	if (c != -1) {
+		if (c != 0) {
+			char links[c][256];
 
-	char links[c][256];
+			FILE *f = fopen("yt.txt", "r");
+			int i = 0;
+				char link[256];
+				while ( fgets(link, 256, f) ) {
+					strcpy(link, strtok(link, "&"));
+					strcpy(links[i], link);
+					i++;
+				}
 
-	FILE *f = fopen("yt.txt", "r");
-	int i = 0;
-		char link[256];
-		while ( fgets(link, 256, f) ) {
-			strcpy(link, strtok(link, "&"));
-			strcpy(links[i], link);
-			i++;
+			int nlinks = sizeof(links) / sizeof(links[0]);
+
+			for (int j=0;j<nlinks;j++) {
+				pthread_create(NULL, NULL, download, links[j]);
+			}
+			pthread_exit(NULL);
+
+			fclose(f);
+		} else {
+			printf("No links");
+			return -1;
 		}
-
-		int nlinks = sizeof(links) / sizeof(links[0]);
-
-		for (int j=0;j<nlinks;j++) {
-			pthread_create(NULL, NULL, download, links[j]);
-		}
-		pthread_exit(NULL);
-
-		fclose(f);
 	} else {
 		printf("File error");
 		return -1;
